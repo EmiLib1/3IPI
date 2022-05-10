2c51aba363be8faf71f5fac5374ab5dd935b7f72
Emerson Rocha <52533818+Emerson-Rocha@users.noreply.github.com>
2022-05-10 12:19:47 -0300

    Continuaremos com mais conteúdo, parte 1

---
+++ formulario01.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- POST - 
+         GET - 
+         -->
+    <form action="processa.php" method="post" enctype="multipart/form-data">
+        <div>
+         <label> nome: </label>
+         <input type="text" name="nome" id="nome">
+        </div> 
+        <div>
+        <div>
+         <label> e-mail: </label>
+         <input type="email" name="email" id="email">
+        </div> 
+        <div>
+         <label> sexo: </label>
+         <input type="radio" name="sexo" id="sexo" value="M"> Masculino
+         <input type="radio" name="sexo" id="sexo" value="F"> Feminino
+        </div> 
+         
+
+
+
+        <div>
+
+             <input type="submit" value="enviar"/>
+        </div>
+     </form>
+</body>
+</html>
